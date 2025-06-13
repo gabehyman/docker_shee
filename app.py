ee0a33cbb239294a347124784d26dd46cf3c23f3
gabehyman <62864199+gabehyman@users.noreply.github.com>
2025-06-12 22:43:55 -0400

    gon get ugly

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import datetime
 import html
 
 app = Flask(__name__, static_folder="static", static_url_path="/static")
-logs = []  # this will store all messages
+logs = []  # this will store all shee
 
 @app.route("/")
 def home():
