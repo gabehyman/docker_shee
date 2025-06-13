5419292dd5d12a5daaee1a6c9147cc6ceab5f8bf
gabehyman <62864199+gabehyman@users.noreply.github.com>
2025-06-12 22:41:28 -0400

    de nuevo

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import datetime
 import html
 
 app = Flask(__name__, static_folder="static", static_url_path="/static")
-logs = []  # this will store all messages and outputs
+logs = []  # this will store all messages
 
 @app.route("/")
 def home():
