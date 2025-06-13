692f9000455f3a67cc3e54854f7bbb60f373904c
gabehyman <62864199+gabehyman@users.noreply.github.com>
2025-06-12 23:57:09 -0400

    yup

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import datetime
 import html
 
 app = Flask(__name__, static_folder="static", static_url_path="/static")
-logs = []  # this will store all 
+logs = []  # this will store all dawg
 
 @app.route("/")
 def home():
